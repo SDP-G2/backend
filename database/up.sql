fae655198e3618a66b0166825950c310f0055e49
Kyle Cotton <kylecottonkc@gmail.com>
2021-02-12 13:25:36 +0000

    Create Commands table

--- database/up.sql
+++ database/up.sql
@@ -9,3 +9,12 @@ CREATE TABLE IF NOT EXISTS Users (
        password_hash VARCHAR NOT NULL,
        robot_serial_number VARCHAR NOT NULL UNIQUE REFERENCES Robot(robot_serial_number)
 );
+
+CREATE TABLE IF NOT EXISTS Commands (
+       command_id BIGSERIAL PRIMARY KEY,
+       robot_serial_number VARCHAR NOT NULL REFERENCES Robot(robot_serial_number),
+       time_issued timestamptz NOT NULL,
+       time_instruction timestamptz NOT NULL,
+       instruction VARCHAR NOT NULL,
+       completed boolean NOT NULL DEFAULT FALSE
+);
