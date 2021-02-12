276689597721ce3c602cba68960e6a3f29cef98f
Kyle Cotton <kylecottonkc@gmail.com>
2021-02-12 13:25:17 +0000

    Create Robot table

--- database/up.sql
+++ database/up.sql
@@ -1,3 +1,8 @@
+CREATE TABLE IF NOT EXISTS Robot (
+       robot_serial_number VARCHAR PRIMARY KEY,
+       battery_level BIGINT NOT NULL DEFAULT 0
+);
+
 CREATE TABLE IF NOT EXISTS Users (
        user_id BIGSERIAL PRIMARY KEY,
        user_name VARCHAR NOT NULL UNIQUE,
