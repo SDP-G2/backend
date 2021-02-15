a2668d057189c2853dffa917445a22cccb38456f
Kyle Cotton <kylecottonkc@gmail.com>
2021-02-15 11:22:22 +0000

    Allow the user to create a new task
    Allow the user to set the robot to perform a cleaning operation

--- sdp-backend/src/command.rs
+++ sdp-backend/src/command.rs
@@ -93,8 +93,67 @@ impl Command {
             completed: false,
         })
     }
+
+    // Get the current task the robot is doing
+    pub async fn current(conn: &PgPool, robot_serial_number: &str) -> Result<Self, ApiError> {
+        sqlx::query!(
+            r#"
+SELECT * FROM Commands C
+NATURAL JOIN
+(SELECT MAX(C1.time_issued) AS time_issued,
+        $1 AS robot_serial_number
+FROM Commands C1
+WHERE C1.robot_serial_number = $1) MaxTimeIssued
+               "#,
+            robot_serial_number
+        )
+        .fetch_one(conn)
+        .await
+        .map(|cmd| Self {
+            command_id: cmd.command_id,
+            robot_serial_number: cmd.robot_serial_number,
+            time_issued: cmd.time_issued,
+            time_instruction: cmd.time_issued,
+            instruction: Instruction::Idle,
+            completed: cmd.completed,
+        })
+        .map_err(|e| {
+            println!("Command Latest: {:?}", e);
+            ApiError::DatabaseConnFailed
+        })
+    }
+
+    // pub async fn update(
+    //     conn: &PgPool,
+    //     robot_serial_number: &str,
+    //     time_issued: chrono::DateTime<Utc>,
+    //     time_instruction: chrono::DateTime<Utc>,
+    //     instruction: &Instruction,
+    // ) -> Result<Self, ApiError> {
+    //     todo!();
+    // }
 }
+
 impl Command {
+    // Abort the current task with the given reason
+    pub async fn abort(
+        conn: &PgPool,
+        robot_serial_number: &str,
+        reason: &AbortReason,
+    ) -> Result<Self, ApiError> {
+        // Create a new command with the current time
+        let time_now = chrono::Utc::now();
+
+        Ok(Command::new(
+            conn,
+            robot_serial_number,
+            time_now,
+            time_now,
+            &Instruction::Abort(reason.clone()),
+        )
+        .await?)
+    }
+
     // Idle task the current task with the given reason
     pub async fn idle(conn: &PgPool, robot_serial_number: &str) -> Result<Self, ApiError> {
         // Create a new command with the current time
@@ -106,6 +165,24 @@ impl Command {
             time_now,
             time_now,
             &Instruction::Idle,
+        )
+        .await?)
+    }
+
+    pub async fn task(
+        conn: &PgPool,
+        robot_serial_number: &str,
+        cleaning_pattern: &CleaningPattern,
+    ) -> Result<Self, ApiError> {
+        // Create a new command with the current time
+        let time_now = chrono::Utc::now();
+
+        Ok(Command::new(
+            conn,
+            robot_serial_number,
+            time_now,
+            time_now,
+            &Instruction::Task(cleaning_pattern.clone()),
         )
         .await?)
     }
