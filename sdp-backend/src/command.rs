2fccc78c31fce396cd64152cd06a56cada0b3f4f
Kyle Cotton <kylecottonkc@gmail.com>
2021-03-07 14:57:35 +0000

    Added Status enum
    This is used to represent all of the possible states a command could
    be in.

--- sdp-backend/src/command.rs
+++ sdp-backend/src/command.rs
@@ -18,7 +18,22 @@ pub struct Command {
     #[serde(with = "ts_seconds")]
     time_instruction: chrono::DateTime<Utc>,
     pub instruction: Instruction,
-    pub completed: bool,
+    pub status: Status,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+pub enum Status {
+    // A command that has been submitted to the system but not started
+    Pending,
+    // The command the robot is currently doing
+    InProgress,
+    // The command completed successfully
+    Completed,
+    // The command has been paused due to an obstacle
+    Paused,
+    // The command has beenc ancelled due to an abort
+    Cancelled,
+}
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
