a3e9a99863e9253087b7a30cac96ab601dc14b51
Kyle Cotton <kylecottonkc@gmail.com>
2021-03-07 15:17:40 +0000

    A Robot is retirved by its robot_serial_number

--- sdp-backend/src/robot.rs
+++ sdp-backend/src/robot.rs
@@ -26,6 +26,22 @@ VALUES ($1)
         .map_err(|_| ApiError::RobotInitializationFailed)
     }
 
+    pub async fn get_by_serial(conn: &PgPool, robot_serial_number: &str) -> Result<Self, ApiError> {
+        sqlx::query!(
+            r#"
+SELECT * FROM Robot R
+WHERE R.robot_serial_number = $1
+               "#,
+            robot_serial_number
+        )
+        .fetch_one(conn)
+        .await
+        .map(|r| Self {
+            robot_serial_number: r.robot_serial_number,
+            assigned: r.assigned,
+        })
+        .map_err(|_| ApiError::DatabaseConnFailed)
+    }
 
         // By default a new robot will be in idle.
         // Command::idle(conn, robot_serial_number).await?;
