435996910fa7f783192f42840923879748d6a393
Kyle Cotton <kylecottonkc@gmail.com>
2021-03-10 14:46:30 +0000

    Added prune function

--- sdp-backend/src/command/retrieve.rs
+++ sdp-backend/src/command/retrieve.rs
@@ -56,6 +56,34 @@ WHERE C.robot_serial_number = $1 AND
         Ok(commands)
     }
 
+    // Cancel all of the commands that do not call in the
+    async fn prune(conn: &PgPool, commands: Vec<Self>) -> Result<Vec<Self>, ApiError> {
+        let mut pruned_commands = Vec::new();
+        let time_now = chrono::Utc::now();
+        for c in commands {
+            // Check for errors in the time instruction
+            let (_, time_instruction_error) = c.valid_time();
+
+            match time_instruction_error {
+                // If the command is scheduled for the future that is fine
+                _ if c.time_instruction > time_now => pruned_commands.push(c),
+
+                // If the command is inside the time buffer, its valid
+                None => pruned_commands.push(c),
+
+                // There was an error with the command, cancel it
+                Some(_) => {
+                    c.cancel(conn).await?;
+                    ()
+                }
+            }
+        }
+
+        pruned_commands.sort_by(|a, b| a.time_instruction.cmp(&b.time_instruction));
+
+        Ok(pruned_commands)
+    }
+
     // Returns the command that should be executed next, or None if isn't anything
     // Prune the commands, if the time has expired and they have not been completed mark them as cancelled
     // Check that the command is in the time buffer before sending it.
@@ -66,31 +94,71 @@ WHERE C.robot_serial_number = $1 AND
         // Get all of the pending commands for this robot
         let all_pending_commands = Self::get_all_pending(conn, robot_serial_number).await?;
 
-        // If the commands time_instruiction time is outside of the time buffer
-        // mark it as cancelled.
-        let mut commands = Vec::new();
-        for c in all_pending_commands {
-            // Check for errors in the time instruction
-            let (_, time_instruction_error) = Self::check_time(c.time_issued, c.time_instruction);
-
-            // If there is an error with the instruction time cancel it
-            // otherwise add it to the list
-            match time_instruction_error {
-                None => commands.push(c),
-                Some(_) => {
-                    c.cancel(conn).await?;
-                    ()
-                }
-            }
-        }
-
-        // Get the command that should be run text
-        // The command with the first time_instruiction
-        commands.sort_by(|a, b| a.time_instruction.cmp(&b.time_instruction));
+        // Cancel all of the invalid commands and get the earliest one
+        let commands = Self::prune(conn, all_pending_commands).await?;
 
         match commands.get(0) {
-            Some(c) => Ok(Some(c.clone())),
-            None => Ok(None),
+            Some(c) if c.instruction_in_buffer() => {
+                println!("{:?}", c);
+                Ok(Some(c.clone()))
+            }
+            _ => Ok(None),
+        }
+    }
+
+    // Get all of the commands that are currently ongoing, that is have a status that is paused or
+    // in progress
+    pub async fn ongoing(conn: &PgPool, robot_serial_number: &str) -> Result<Vec<Self>, ApiError> {
+        let results = sqlx::query!(
+            r#"
+SELECT * FROM Commands C
+WHERE C.robot_serial_number = $1 AND
+     (C.status = 'Status::Paused' OR C.status = 'Status::InProgress')
+               "#,
+            robot_serial_number
+        )
+        .fetch_all(conn)
+        .await
+        .map_err(|_| ApiError::DatabaseConnFailed)?;
+
+        let mut all_ongoing_commands = Vec::new();
+        for c in results {
+            all_ongoing_commands.push(Self {
+                command_id: c.command_id,
+                robot_serial_number: c.robot_serial_number,
+                time_issued: c.time_issued,
+                time_instruction: c.time_issued,
+                instruction: c.instruction.into(),
+                status: c.status.into(),
+            })
+        }
+
+        Ok(all_ongoing_commands)
+    }
+
+    pub async fn cancel_all_ongoing(
+        conn: &PgPool,
+        robot_serial_number: &str,
+    ) -> Result<(), ApiError> {
+        // Get all of the commands that are ongoing and cancel them
+        for c in Self::ongoing(conn, robot_serial_number).await? {
+            c.cancel(conn).await?;
+        }
+
+        Ok(())
+    }
+
+    pub async fn init_command(conn: &PgPool, robot_serial_number: &str) -> Result<Self, ApiError> {
+        // Cancel all of the ongoing commands
+        Self::cancel_all_ongoing(conn, robot_serial_number).await?;
+
+        // If there is a pending command use that, otherwise make a new idle command
+        match Self::pending(conn, robot_serial_number).await? {
+            Some(c) => Ok(c),
+            None => Ok(Self::new_idle(conn, robot_serial_number)
+                .await?
+                .in_progress(conn)
+                .await?),
         }
     }
 }
