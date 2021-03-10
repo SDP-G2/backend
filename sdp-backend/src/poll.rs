21393cfaed66d5cf58ca93f74ffb353df890eba3
Kyle Cotton <kylecottonkc@gmail.com>
2021-03-10 14:48:01 +0000

    Updated polling logic

--- sdp-backend/src/poll.rs
+++ sdp-backend/src/poll.rs
@@ -26,30 +26,45 @@ impl Poll {
         // Get the current command from the database
         let current_command = match Command::get_by_id(conn, poll.command_id).await {
             Ok(c) if c.status.cancelled() => {
-                return Command::pending(conn, &poll.robot_serial_number)
-                    .await
-                    .map(|c| c.expect("The abort command should have been inserted"));
+                match Command::pending(conn, &poll.robot_serial_number).await? {
+                    Some(abort) => return Ok(abort),
+                    _ => return Err(ApiError::CmdInstructionNotSupported),
+                }
             }
             Ok(c) => c,
             Err(e) => return Err(e),
         };
 
         match &current_command.instruction {
-            Task(_) => {
-                current_command
-                    .update_status(conn, &poll.current_command_status)
-                    .await
+            // If we are doing a task and it had not completed,
+            // update the status and keep doing it
+            Task(_) if !poll.status.is_completed() => {
+                current_command.update_status(conn, &poll.status).await
             }
-            Idle if !poll.check_battery().await => current_command.low_battery_abort(conn).await,
-            Idle => match Command::pending(conn, &poll.robot_serial_number).await? {
-                // There is a pennding command, mark the current command as completed
-                // then start the other task and return it
-                Some(cmd) => {
-                    current_command.completed(conn).await?;
-                    cmd.in_progress(conn).await?;
-                    Ok(cmd)
+
+            // If we are doing a task and it has completed do the next
+            Task(_) if poll.status.is_completed() => {
+                current_command.update_status(conn, &poll.status).await?;
+                match Command::pending(conn, &poll.robot_serial_number).await? {
+                    Some(c) => Ok(c.in_progress(conn).await?),
+                    None => {
+                        Command::new_idle(conn, &poll.robot_serial_number)
+                            .await?
+                            .in_progress(conn)
+                            .await
+                    }
                 }
-                // There are no commands pending, keep doing the idle command
+            }
+
+            // If we are idle check that the battery level is valid
+            Idle if !poll.check_battery().await => current_command.low_battery_abort(conn).await,
+
+            // If we are idle and battery is ok check for commands, if there are non stay idle
+            Idle => match Command::pending(conn, &poll.robot_serial_number).await? {
+                Some(c) => {
+                    current_command.completed(conn).await?;
+                    Ok(c.in_progress(conn).await?)
+                }
                 None => Ok(current_command),
             },
             _unsupported => Err(ApiError::CmdInstructionNotSupported),
@@ -61,27 +76,37 @@ impl Poll {
     /// If the battery level is not sufficent the robot will
     /// be told to abort due to low battery.
     async fn check_battery(&self) -> bool {
-        self.battery_level >= 0
-            && self.battery_level > MINIMUM_BATTERY_LEVEL
-            && self.battery_level <= 100
+        check_battery(self.battery_level)
     }
+}
+
+fn check_battery(battery_level: i64) -> bool {
+    (battery_level >= 0 && battery_level <= 100) && (battery_level > MINIMUM_BATTERY_LEVEL)
 }
 
 // When the robot first turns on it will have no knowledge of previous communications
 impl Init {
     pub async fn init(conn: &PgPool, init: &Self) -> Result<Command, ApiError> {
-        let polling_command = match Command::pending(conn, &init.robot_serial_number).await? {
-            Some(c) => c,
-            None => Command::new_idle(conn, &init.robot_serial_number).await?,
-        };
+        if !init.check_battery() {
+            return Command::new_low_battery(conn, &init.robot_serial_number).await;
+        }
+
+        let polling_command = Command::init_command(conn, &init.robot_serial_number).await?;
+        println!("Init Command {:?}", polling_command);
+
+        polling_command.in_progress(conn).await?;
 
         let poll = Poll {
             robot_serial_number: init.robot_serial_number.clone(),
-            current_command_id: polling_command.command_id,
-            current_command_status: polling_command.status,
+            command_id: polling_command.command_id,
+            status: polling_command.status,
             battery_level: init.battery_level,
         };
 
         Poll::poll(conn, &poll).await
+    }
+
+    fn check_battery(&self) -> bool {
+        check_battery(self.battery_level)
     }
 }
